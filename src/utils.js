55343d93e4b25db0f2cb9d80688a10a5e4793dde
Haoran Zhou <kens0serious@gmail.com>
2022-02-09 20:44:55 -0500

    paginate function

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,11 @@
-const paginate = () => {}
+const paginate = (data) => {
+  const itemPerPage = 10
+  const pages = Math.ceil(data.length / itemPerPage)
+
+  const newData = Array.from({ length: pages }, (_, index) => {
+    const start = index * itemPerPage
+    return data.slice(start, start + itemPerPage)
+  })
+}
 
 export default paginate
